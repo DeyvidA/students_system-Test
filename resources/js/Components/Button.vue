b04d22fdd61c43c9c63a6282a5aa635a8f9c9ddd
DeyvidA <darauz@nicarao.agency>
2024-03-11 06:03:29 -0600

    create button controller

---
+++ resources/js/Components/Button.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+    type: {
+        type: String,
+        default: "button",
+    },
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
+    onClick: {
+        type: Function,
+        default: () => {},
+    },
+});
+</script>
+
+<template>
+    <button
+        :type="type"
+        :disabled="disabled"
+        :class="`w-full rounded-md py-2 px-4 text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-opacity-50`"
+        @click="onClick"
+    >
+        <slot />
+    </button>
+</template>
